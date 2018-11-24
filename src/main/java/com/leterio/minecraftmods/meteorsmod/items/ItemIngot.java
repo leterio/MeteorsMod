6a589c12694529e4ddf997595682b6da0748df06
Vinícius Letério <viniciusleterio@gmail.com>
2018-11-24 21:10:22 -0200

    Rename BaseItem->ItemBase e inclusão dos lingotes

---
+++ src/main/java/com/leterio/minecraftmods/meteorsmod/items/ItemIngot.java
@@ -0,0 +1,9 @@
+package com.leterio.minecraftmods.meteorsmod.items;
+
+import com.leterio.minecraftmods.meteorsmod.MeteorsMod;
+
+public class ItemIngot extends ItemBase {
+	public ItemIngot(String itemName) {
+		super(itemName, 64, MeteorsMod.meteorsCTab);
+	}
+}
