ca85d9a2765d95eaa91841a31c8e1e007abcffba
Vinícius Letério <viniciusleterio@gmail.com>
2018-11-25 15:42:14 -0200

    Tooltips nos items

--- src/main/java/com/leterio/minecraftmods/meteorsmod/items/ItemBase.java
+++ src/main/java/com/leterio/minecraftmods/meteorsmod/items/ItemBase.java
@@ -1,9 +1,18 @@
 package com.leterio.minecraftmods.meteorsmod.items;
 
+import java.util.List;
+
 import com.leterio.minecraftmods.meteorsmod.MeteorsMod;
 
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import scala.actors.threadpool.Arrays;
 
 public class ItemBase extends Item {
 	public ItemBase(final String itemName, final int maxStackSize, final CreativeTabs creativeTab) {
@@ -13,5 +22,16 @@ public class ItemBase extends Item {
 		setCreativeTab(creativeTab);
 
 		Items.ITEMS.add(this);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@SuppressWarnings("unchecked")
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		String key = this.getUnlocalizedName() + ".tooltip";
+		String tooltipString;
+		if (I18n.hasKey(key) && (tooltipString = I18n.format(key)).length() > 0) {
+			tooltip.addAll(Arrays.asList(tooltipString.split(System.lineSeparator())));
+		}
 	}
 }
--- src/main/java/com/leterio/minecraftmods/meteorsmod/items/ItemFood.java
+++ src/main/java/com/leterio/minecraftmods/meteorsmod/items/ItemFood.java
@@ -1,8 +1,17 @@
 package com.leterio.minecraftmods.meteorsmod.items;
 
+import java.util.List;
+
 import com.leterio.minecraftmods.meteorsmod.MeteorsMod;
 
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import scala.actors.threadpool.Arrays;
 
 public class ItemFood extends net.minecraft.item.ItemFood {
 	public ItemFood(final String itemName, final int amount, int maxStackSize, CreativeTabs creativeTab) {
@@ -15,4 +24,14 @@ public class ItemFood extends net.minecraft.item.ItemFood {
 		Items.ITEMS.add(this);
 	}
 
+	@SideOnly(Side.CLIENT)
+	@SuppressWarnings("unchecked")
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		String key = this.getUnlocalizedName() + ".tooltip";
+		String tooltipString;
+		if (I18n.hasKey(key) && (tooltipString = I18n.format(key)).length() > 0) {
+			tooltip.addAll(Arrays.asList(tooltipString.split("\n")));
+		}
+	}
 }
