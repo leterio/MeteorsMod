1d29530a15653a33b7edf655b2c7d25d707770e3
Vinícius Letério <viniciusleterio@gmail.com>
2019-01-26 23:48:18 -0200

    Encantamento Cold Touch refeito, baseado no Frost Walker (Vanilla)

--- src/main/java/com/leterio/minecraftmods/meteorsmod/handlers/ColdTouchPlayerTickHandler.java
+++ src/main/java/com/leterio/minecraftmods/meteorsmod/handlers/ColdTouchPlayerTickHandler.java
@@ -4,15 +4,20 @@ import java.util.Iterator;
 
 import com.leterio.minecraftmods.meteorsmod.enchantment.Enchantments;
 
+import net.minecraft.block.BlockLiquid;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -25,39 +30,42 @@ public class ColdTouchPlayerTickHandler {
 		EntityPlayer player = event.player;
 		World world = player.world;
 
-		int totalLevel = getPower(player);
-		if (totalLevel > 0 && !player.isInWater() && event.side.isServer()) {
-			Vec3d playerPos = player.getPositionVector();
-			int l = (int) Math.round(playerPos.x);
-			int y = (int) playerPos.y - 1;
-			int k1 = (int) Math.round(playerPos.z);
-			for (int x = l - totalLevel; x < l + totalLevel; x++) {
-				for (int z = k1 - totalLevel; z < k1 + totalLevel; z++) {
-					BlockPos pos = new BlockPos(x, y, z);
-					if ((world.getBlockState(pos).getBlock() == Blocks.WATER) || (world.getBlockState(pos).getBlock() == Blocks.FLOWING_WATER)) {
-						world.setBlockState(pos, Blocks.ICE.getDefaultState());
+		if (event.side.isServer() && !player.isInWater() && !player.isSneaking() && hasEnchantment(player) && player.fallDistance < 6) {
+			BlockPos pos = new BlockPos(player);
+
+			float factor = 3;
+			BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos(0, 0, 0);
+
+			for (BlockPos.MutableBlockPos blockpos$mutableblockpos1 : BlockPos.getAllInBoxMutable(pos.add((double) (-factor), -1.0D, (double) (-factor)), pos.add((double) factor, -1.0D, (double) factor))) {
+				if (blockpos$mutableblockpos1.distanceSqToCenter(player.posX, player.posY, player.posZ) <= (double) (factor * factor)) {
+					blockpos$mutableblockpos.setPos(blockpos$mutableblockpos1.getX(), blockpos$mutableblockpos1.getY() + 1, blockpos$mutableblockpos1.getZ());
+					IBlockState iblockstate = world.getBlockState(blockpos$mutableblockpos);
+
+					if (iblockstate.getMaterial() == Material.AIR) {
+						IBlockState iblockstate1 = world.getBlockState(blockpos$mutableblockpos1);
+
+						if (iblockstate1.getMaterial() == Material.WATER && (iblockstate1.getBlock() == net.minecraft.init.Blocks.WATER || iblockstate1.getBlock() == net.minecraft.init.Blocks.FLOWING_WATER)
+								&& ((Integer) iblockstate1.getValue(BlockLiquid.LEVEL)).intValue() == 0 && world.mayPlace(Blocks.FROSTED_ICE, blockpos$mutableblockpos1, false, EnumFacing.DOWN, (Entity) null)) {
+							world.setBlockState(blockpos$mutableblockpos1, Blocks.FROSTED_ICE.getDefaultState());
+							world.scheduleUpdate(blockpos$mutableblockpos1.toImmutable(), Blocks.FROSTED_ICE, MathHelper.getInt(player.getRNG(), 60, 120));
+						}
 					}
 				}
 			}
+
 		}
 	}
 
-	public static int getPower(EntityPlayer player) {
-		int power = 0;
-		ItemStack[] itemsOnPlayer = getHeldItems(player);
-		for (int i = 0; i < itemsOnPlayer.length; i++) {
-			Iterator<NBTBase> itr = itemsOnPlayer[i].getEnchantmentTagList().iterator();
+	public static boolean hasEnchantment(EntityPlayer player) {
+		ItemStack feet = player.getItemStackFromSlot(EntityEquipmentSlot.FEET);
+		if (feet != null) {
+			Iterator<NBTBase> itr = feet.getEnchantmentTagList().iterator();
 			while (itr.hasNext()) {
-				NBTTagCompound e = (NBTTagCompound) itr.next();
-				if (Enchantment.getEnchantmentByID((int) e.getInteger("id")) == Enchantments.ENCHANTMENT_COLD_TOUCH) {
-					power += e.getInteger("lvl");
+				if (Enchantment.getEnchantmentByID((int) ((NBTTagCompound) itr.next()).getInteger("id")) == Enchantments.ENCHANTMENT_COLD_TOUCH) {
+					return true;
 				}
 			}
 		}
-		return power < 0 ? 0 : power > 3 ? 3 : power;
-	}
-
-	private static ItemStack[] getHeldItems(EntityPlayer player) {
-		return new ItemStack[] { player.getItemStackFromSlot(EntityEquipmentSlot.FEET) };
+		return false;
 	}
 }
